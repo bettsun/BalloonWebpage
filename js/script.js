25254341b118fe787bf07fcc0c1949a2a7027069
bettsun <80205831+bettsun@users.noreply.github.com>
2021-11-10 10:16:45 +0300

    other files

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const container = document.querySelector(".container");
+
+const colors = ['#6495ed', '#7fffd4', '#ffa07a', '#f08080', '#afeeee'];
+
+let i = 0;
+
+
+document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+	container.classList.add("change");
+})
+
+document.querySelector(".close-navbar-icon").addEventListener("click", () => {
+	container.classList.remove("change");
+})
+
+Array.from(document.querySelectorAll(".nav-link")).forEach(item => {
+	item.style.cssText = `background-color:${colors[i++]}`
+})
+
+Array.from(document.querySelectorAll(".navigation-btn")).forEach((item) => {
+	item.onclick = () => {
+		item.parentElement.parentElement.classList.toggle("change");
+	};
+});
+
+
+
